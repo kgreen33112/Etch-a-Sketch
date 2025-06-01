060c6e3b2d57b1940534b7268bef8d286cc2e8cd
Klayci <kgreencoprods@gmail.com>
2025-06-01 16:29:53 -0500

    Update functionality to ensure color stays

--- app.js
+++ app.js
@@ -4,16 +4,14 @@ function makeGrid(rows, cols) {
     let cell;
     for (let c = 0; c < (rows * cols); c++) {
         cell = document.createElement("div");
-        
         container.appendChild(cell).className = "grid-item";
     };
-
-    const gridItems = document.querySelectorAll('.grid-item');
-    gridItems.forEach(item => {
-        item.addEventListener('mouseover', function() {
-            this.style.backgroundcolor = "purple";
-        });
-    });
 };
 
 makeGrid(16, 16)
+
+document.querySelectorAll('.grid-item').forEach(item => {
+    item.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = "purple";
+    }, false);
+});
