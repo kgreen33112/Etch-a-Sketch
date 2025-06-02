78b16672a48655f55899b63a233e2f99022c9b7a
Klayci <kgreencoprods@gmail.com>
2025-06-01 19:22:33 -0500

    Fixed grid creation

--- app.js
+++ app.js
@@ -1,17 +1,69 @@
-function makeGrid(rows, cols) {
-    const container = document.getElementById("grid-container");
+const container = document.querySelector(".grid-container");
+const button = document.querySelector("#grid-size");
+const resetButton = document.querySelector("#reset");
+const colorBoxes = document.querySelectorAll(".color-container .cell");
+const colors = [
+  "blk",
+  "red",
+  "blue",
+  "green",
+  "yellow",
+  "orange",
+  "purple",
+  "cyan",
+  "pink",
+  "brown",
+];
 
-    let cell;
-    for (let c = 0; c < (rows * cols); c++) {
-        cell = document.createElement("div");
-        container.appendChild(cell).className = "grid-item";
-    };
-};
+let selectedColor = "blk"; // Default color
 
-makeGrid(16, 16)
+button.addEventListener("click", handleClick);
+resetButton.addEventListener("click", resetClick);
 
-document.querySelectorAll('.grid-item').forEach(item => {
-    item.addEventListener('mouseover', (e) => {
-        e.target.style.backgroundColor = "purple";
-    }, false);
-});
+//assign color
+colorBoxes.forEach((box) =>
+  box.addEventListener("click", () => {
+    selectedColor = Array.from(box.classList).find((c) => c != "cell");
+  })
+);
+
+let choice;
+
+function handleClick() {
+  container.innerHTML = "";
+  choice = parseInt(prompt("What size of grid would you like to make? (Range 0-64): "));
+  if (!isNaN(choice) && choice > 0 && choice <= 64) {
+    for (let j = 0; j < choice; j++) {
+      const row = document.createElement("div");
+      row.classList.add("row");
+
+      for (let i = 0; i < choice; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell", "grid-cell");
+        row.appendChild(cell);
+        cell.addEventListener("mouseenter", toggleColor);
+
+        function toggleColor() {
+          colors.forEach((color) => {
+            cell.classList.remove(color);
+          });
+          cell.classList.add(selectedColor);
+        }
+      }
+
+      container.appendChild(row);
+    }
+  } else {
+    alert("Please enter a number from 0-64");
+  }
+}
+
+function resetClick() {
+  const cells = document.querySelectorAll(".grid-cell");
+
+  cells.forEach((cell) => {
+    colors.forEach((color) => {
+      cell.classList.remove(color);
+    });
+  });
+}
